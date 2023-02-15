bdbac3ce3659b65c5684de62aa6afabfb4acce97
Emerson Kaneda <emerson.k337@gmail.com>
2023-02-15 18:03:02 -0300

    CAM without pins define

--- main/main.c
+++ main/main.c
@@ -50,23 +50,23 @@
 
 //***************************************************************//
 
-#define CAM_PIN_PWDN    -1 //power down is not used
-#define CAM_PIN_RESET   -1 //software reset will be performed
-#define CAM_PIN_XCLK    21
-#define CAM_PIN_SIOD    26
-#define CAM_PIN_SIOC    27
+#define CAM_PIN_PWDN -1  // power down is not used
+#define CAM_PIN_RESET -1 // software reset will be performed
+#define CAM_PIN_XCLK
+#define CAM_PIN_SIOD
+#define CAM_PIN_SIOC
 
-#define CAM_PIN_D7      35
-#define CAM_PIN_D6      34
-#define CAM_PIN_D5      39
-#define CAM_PIN_D4      36
-#define CAM_PIN_D3      19
-#define CAM_PIN_D2      18
-#define CAM_PIN_D1       5
-#define CAM_PIN_D0       4
-#define CAM_PIN_VSYNC   25
-#define CAM_PIN_HREF    23
-#define CAM_PIN_PCLK    22
+#define CAM_PIN_D7
+#define CAM_PIN_D6
+#define CAM_PIN_D5
+#define CAM_PIN_D4
+#define CAM_PIN_D3
+#define CAM_PIN_D2
+#define CAM_PIN_D1
+#define CAM_PIN_D0
+#define CAM_PIN_VSYNC
+#define CAM_PIN_HREF
+#define CAM_PIN_PCLK
 
 #define MICROSTEP 1
 #define MAX_STEP 4096
